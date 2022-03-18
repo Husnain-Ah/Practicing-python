fb0bc7a1509013736218206db9aee0c304b9e132
Husnain Ahmed <70273018+Husnain-Ah@users.noreply.github.com>
2022-03-18 13:55:24 +0000

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 # if input is divisible by 3 output fizz, if it is divisible by 5 output buzz, if divisible by both output fizzbuzz
 
-def fizz_buzz(input):
+def fizzbuzz(input):
 
     if input % 3  == 0 and input % 5 == 0: # I had to move this statement to the top as the value of 15 was being caught on the first statement of 'divisible by 3'
         output = "fizzbuzz"
@@ -10,4 +10,4 @@ def fizz_buzz(input):
         output = "buzz"
     return output
     
-print(fizz_buzz(5))
+print(fizzbuzz(15))
