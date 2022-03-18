0a5f0d2a5356028bb459d278abfe534e537dcc42
Husnain Ahmed <70273018+Husnain-Ah@users.noreply.github.com>
2022-03-18 10:14:58 +0000

    Create basic calculator not working.py

---
+++ basic calculator not working.py
@@ -0,0 +1,7 @@
+num1 = input('enter your first number : ')
+func = input('enter your function from the following : +, -, *, / ')
+num2 = input('enter your second number : ')
+calc = (num1, func, num2)
+print (calc)
+
+# not working just outputs your input data
