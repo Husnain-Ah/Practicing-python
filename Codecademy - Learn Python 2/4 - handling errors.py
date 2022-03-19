01fe0ffe573f44bc9410ed6241725e55c754bc13
Husnain Ahmed <70273018+Husnain-Ah@users.noreply.github.com>
2022-03-19 12:11:23 +0000

    first 7 tasks
    7/14

---
+++ Codecademy - Learn Python 2/4 - handling errors.py
@@ -0,0 +1,3 @@
+print("How do you make a hot dog stand?")
+print("You take away its chair!")
+# make sure that the quotes match,  "" or '', no mixing and matching
---
+++ Codecademy - Learn Python 2/6 - arithmetic.py
@@ -0,0 +1,4 @@
+x = 9
+y = 7
+product = x*y
+remainder = 1398%11
