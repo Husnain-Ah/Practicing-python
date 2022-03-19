2b2799b68842779d137275a1e890822ba20706de
Husnain Ahmed <70273018+Husnain-Ah@users.noreply.github.com>
2022-03-19 12:24:46 +0000

    Create 10 - two types of division.py

---
+++ Codecademy - Learn Python 2/10 - two types of division.py
@@ -0,0 +1,18 @@
+cucumbers = 100
+num_people = 6
+
+whole_cucumbers_per_person = cucumbers / num_people
+print(whole_cucumbers_per_person)
+
+float_cucumbers_per_person = (float(cucumbers)/num_people)
+print(float_cucumbers_per_person)
+
+
+quotient1 = 7./2
+# the value of quotient1 is 3.5
+quotient2 = 7/2.
+# the value of quotient2 is 3.5
+quotient3 = 7./2.
+# the value of quotient3 is 3.5
+
+# this is all python 2 syntax so its kind of irrelevant - just remember to initialise a datatype
