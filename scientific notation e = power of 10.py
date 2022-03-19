c878a37d4b62ba2bc69a9344d1a2364e4da26d7f
Husnain Ahmed <70273018+Husnain-Ah@users.noreply.github.com>
2022-03-19 12:20:43 +0000

    Create scientific notation e = power of 10.py

---
+++ scientific notation e = power of 10.py
@@ -0,0 +1,18 @@
+# scientific notation e = power of 10
+
+# this evaluates to 150:
+float4 = 1.5e2 
+
+print (int(float4))
+
+x = 2e3
+print (x)
+
+y = 2e4
+print (y)
+
+z = 2e1
+print (z)
+
+# Python's power operator is ** and Euler's number is math.e
+# https://stackoverflow.com/questions/39154611/how-can-i-use-e-eulers-number-and-power-operation-in-python-2-7  check this out later and do research on the math library
