5e995add781feac2e5163496da71e50eb3f6c706
Husnain Ahmed <70273018+Husnain-Ah@users.noreply.github.com>
2022-04-05 15:11:03 +0100

    Create 10 - Mix 'n' Match.py

---
+++ Codecademy - Learn Python 2/3 - Conditionals and Control Flow/L1 - Conditionals & Control Flow/10 - Mix 'n' Match.py
@@ -0,0 +1,16 @@
+# Use boolean expressions as appropriate on the lines below!
+
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
+
+# Make me true!
+bool_two = not 7==5
+
+# Make me false!
+bool_three = 6==7 or 2>7
+
+# Make me true!
+bool_four = 2<3 and 7>6
+
+# Make me true!
+bool_five = not 6<5
