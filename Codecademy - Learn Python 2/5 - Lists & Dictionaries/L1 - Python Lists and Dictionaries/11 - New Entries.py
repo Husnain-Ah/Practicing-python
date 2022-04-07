92cee6dec66eaf674e21b4411c597ca5b353891e
Husnain Ahmed <70273018+Husnain-Ah@users.noreply.github.com>
2022-04-07 11:10:05 +0100

    Create 11 - New Entries.py

---
+++ Codecademy - Learn Python 2/5 - Lists & Dictionaries/L1 - Python Lists and Dictionaries/11 - New Entries.py
@@ -0,0 +1,13 @@
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+menu['kebab'] = 5.00
+menu['rice'] = 3.50
+menu['burger'] = 4.99
+
+
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
